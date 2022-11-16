685e5a5b931880955d6bbeba3e8a1e67f34870ab
Pedro Soares <ngrpedro22@gmail.com>
2022-11-16 14:53:46 -0300

    data+types

---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+  { contact: "Contact Name", cash: "in", value: 100.0 },
+  { contact: "Contact Name", cash: "out", value: 10.0 },
+  { contact: "Contact Name", cash: "in", value: 6.5 },
+  { contact: "Contact Name", cash: "out", value: 18.25 },
+  { contact: "Contact Name", cash: "in", value: 5.0 },
+];
---
+++ src/types/Item.ts
@@ -0,0 +1,5 @@
+export type Item = {
+  contact: string;
+  cash: string;
+  value: number;
+};
